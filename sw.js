eb47d87b98f289c54f76aeecb277ed2b2e0b5dad
ytlim1 <ytlim1@gmail.com>
2026-03-03 14:29:57 +1100

    Repo created by uploading files.

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'lunar-v1';
+const ASSETS = [
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.min.js'
+];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
