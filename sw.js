e1ecea31cd5d7235f14b8ccd29dba9ae3283318f
ytlim1 <ytlim1@gmail.com>
2026-03-11 13:57:11 +1100

    Update sw.js - updated for new features.

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
 const CACHE_NAME = 'lunar-v3';
 const ASSETS = [
-  './index.html',
+  'index.html',
+  'manifest.json',
   'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.min.js'
 ];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
 
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
-
-
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
