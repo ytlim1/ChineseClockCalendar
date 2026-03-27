479a7a2752350a5b5ae19b4209673a5c39cdc5eb
ytlim1 <ytlim1@gmail.com>
2026-03-27 14:51:27 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'lunar-clock-2026-03-27-v4';
+const CACHE_NAME = 'lunar-clock-2026-03-27-v6';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  './screenshot-mobile.png',
-  './screenshot-desktop.png',
+  './screenshot_mobile.png',
+  './screenshot_wide.png',
   'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.min.js'
 ];
 
